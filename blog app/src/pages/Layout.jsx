53e828c97e21c688f9e70c9a47d06ef72e6b641f
Prashant <pra4093@gmail.com>
2024-08-15 16:52:43 +0530

    added

--- blog app/src/pages/Layout.jsx
+++ blog app/src/pages/Layout.jsx
@@ -1,11 +1,10 @@
 /* eslint-disable no-unused-vars */
 import React,{useEffect, useState} from 'react';
 import { Link, Outlet,useNavigate } from 'react-router-dom';
-import Header from '../Components/header';
+import Header from '../Components/Header';
 
 
 const Layout = () => {
-
 
   return (
     <div className="min-h-screen flex flex-col">
