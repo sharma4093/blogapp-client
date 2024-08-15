91828c1de1bfe6ab3062197ee082b89200fb6287
Prashant <pra4093@gmail.com>
2024-08-15 21:33:04 +0530

    updated layoute

--- blog app/src/App.jsx
+++ blog app/src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/login";
 import Signup from "./pages/Signup";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Layout from "./pages/Layout";
 import MyBlogs from "./Components/myBlogs";
 import ProtectedRoute from "./Components/ProtectedRoute";
